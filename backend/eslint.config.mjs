e8f192ed931f5e4d13051273d95cd4ddc23264e2
PsclDev <me@pscl.dev>
2025-03-06 21:48:28 +0100

    chore: upgraded eslint

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,57 @@
+// @ts-check
+import eslint from '@eslint/js';
+import * as importPlugin from 'eslint-plugin-import';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  importPlugin.flatConfigs?.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 5,
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',     // Built-in imports (come from NodeJS)
+            'external',    // npm install packages
+            'internal',    // Imports from within the app
+            ['sibling', 'parent'], // Relative imports
+            'index',       // Imports from index file
+            'object',      // Object imports
+            'type'         // Type imports
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc', // Sort in ascending order
+            caseInsensitive: true // Ignore case
+          }
+        }
+      ]
+    },
+  },
+);
